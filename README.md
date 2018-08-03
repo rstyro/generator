010f30c1ad09f48fc1d5eaa0ee2f359b52e75e8a
rstyro <rstyro@github.com>
2018-08-03 12:00:25 +0800

    description
    description

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# generator
+# generator mybatis 逆向工程代码生成器
+### 可以根据 template 这个模板自定义自己的代码生成器
+### 修改generator.xml 里面的配置成自己数据库的信息
+### 运行GeneratorMain.java 的mail 方法即可。
--- generator/src/cn/org/rapid_framework/generator/GeneratorMain.java
+++ generator/src/cn/org/rapid_framework/generator/GeneratorMain.java
@@ -10,7 +10,8 @@ public class GeneratorMain {
 //		g.printAllTableNames();				//打印数据库中的表名称
 		
 		g.deleteOutRootDir();							//删除生成器的输出目录
-		g.generateByTable("buss_config","pm_template");	//通过数据库表生成文件,template为模板的根目录,pm_template 这个模板是符合我项目生成的模板，通用的模板为：nomal_template
+		//第一个参数：是数据库表名，第二个参数是：你选择哪个模板生成代码
+		g.generateByTable("table_name","pm_template");	//通过数据库表生成文件,template为模板的根目录,pm_template 这个模板是符合我项目生成的模板，通用的模板为：nomal_template
 //		g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
 //		g.generateByClass(Blog.class,"template_clazz");
 		 
